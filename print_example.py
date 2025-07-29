164e5999a3d0983e983bc84cdece7649b8daa0e5
dharmishthakothari <dharmishtha.only@gmail.com>
2025-07-29 08:59:26 +0530

    Add files via upload

---
+++ print_example.py
@@ -0,0 +1,6 @@
+name = "juhi"
+number = 100
+print("Hello ",name,end=" ")
+print("Have a good day!!!!","This my python class ",sep="***")
+print("Your name is ",name," Your marks is ",number,sep="-")
+print(number)
---
+++ print_example2.py
@@ -0,0 +1,8 @@
+name='Tops\'s Technolgies'
+print(name)
+msg= "Be a \"strong\""
+print(msg,end="\t")
+name="Tops technol\bogies "
+print(name,end="\t")
+name="Tops tech\tnologies"
+print(name)
