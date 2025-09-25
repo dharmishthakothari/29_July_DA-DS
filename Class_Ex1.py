b5aff664958aaa3edaac7792d0d42d132aae07cc
dharmishthakothari <dharmishtha.only@gmail.com>
2025-09-25 09:02:20 +0530

    Add files via upload

---
+++ Class_Ex1.py
@@ -0,0 +1,15 @@
+#class Person
+class Person:
+
+    #intance methods 
+    def greet(self):
+        print("Good Morning")
+    def bye(self):
+        print("OK Bye...")
+
+#obj is object of Person
+obj=Person()
+obj.greet()
+obj.bye()
+d={"a":1}
+print(d.get("c"))
