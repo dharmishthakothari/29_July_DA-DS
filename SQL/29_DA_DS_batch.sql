9472ec25a7946b30d292ec02a494f148f2304318
dharmishthakothari <dharmishtha.only@gmail.com>
2025-10-11 09:16:52 +0530

    Add files via upload

---
+++ SQL/29_DA_DS_batch.sql
@@ -0,0 +1,12 @@
+-- use sql_da_ds
+/* create table student(student_id int,
+					student_name varchar(20),
+					age int,
+					mobile_num int,
+                    address varchar(200))
+                    */
+-- alter table student add semster_no int
+
+-- drop table student
+
+                    
